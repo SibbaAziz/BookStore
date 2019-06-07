9166cccfa79322baa69d63bf051b8b2a09a579bb
Aziz SIBBA <a.sibba@hotmail.fr>
2019-06-07 02:26:24 +0200

    Commit untracked files

---
+++ BookStore/BookStore.Test/BookStoreTest.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BookStore.Test
+{
+    [TestClass]
+    public class BookStoreTest
+    {
+        [TestMethod]
+        public void GivenBookStore_WhenBuyOneBook_ThenPriceShouldBeThePriceInTheCatalog()
+        {
+            var bookStore = new BookStore();
+            bookStore.Import(json);
+            Assert.AreEqual(8, bookStore.Buy("J.K Rowling - Goblet Of fire"));
+        }
+
+        [TestMethod]
+        public void GivenBookStore_WhenBuyManyBookOfSameCategory_ThenApplyDiscount()
+        {
+            var bookStore = new BookStore();
+            bookStore.Import(json);
+            Assert.AreEqual(18, bookStore.Buy("J.K Rowling - Goblet Of fire", "Robin Hobb - Assassin Apprentice"));
+        }
+
+        [TestMethod]
+        public void GivenBookStore_WhenBuy_ThenOnlyFirtCopyOfCategoryHasDiscount()
+        {
+            var bookStore = new BookStore();
+            bookStore.Import(json);
+            Assert.AreEqual(30, bookStore.Buy("J.K Rowling - Goblet Of fire", "Robin Hobb - Assassin Apprentice", "Robin Hobb - Assassin Apprentice"));
+
+            Assert.AreEqual(69.95, bookStore.Buy("Ayn Rand - FountainHead", "Isaac Asimov - Foundation", "Isaac Asimov - Robot series", "J.K Rowling - Goblet Of fire", "J.K Rowling - Goblet Of fire", "Robin Hobb - Assassin Apprentice", "Robin Hobb - Assassin Apprentice"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotEnoughInventoryException))]
+        public void GivenBookStore_WhenInvalidbasket_ThenShouldThrowException()
+        {
+            var bookStore = new BookStore();
+            bookStore.Import(json);
+            Assert.AreEqual(0, bookStore.Buy("Isaac Asimov - Foundation", "Isaac Asimov - Foundation"));
+        }
+
+        const string json = @"{ 
+   'Category':[
+      {
+         'Name':'Science Fiction',
+         'Discount':0.05
+      },
+      {
+         'Name':'Fantastique',
+         'Discount':0.1
+      },
+      {
+         'Name':'Philosophy',
+         'Discount':0.15
+      }
+   ],
+   'Catalog':[
+      {
+         'Name':'J.K Rowling - Goblet Of fire',
+         'Category':'Fantastique',
+         'Price':8,
+         'Quantity':2
+      },
+      {
+         'Name':'Ayn Rand - FountainHead',
+         'Category':'Philosophy',
+         'Price':12,
+         'Quantity':10
+      },
+      {
+         'Name':'Isaac Asimov - Foundation',
+         'Category':'Science Fiction',
+         'Price':16,
+         'Quantity':1
+      },
+      {
+         'Name':'Isaac Asimov - Robot series',
+         'Category':'Science Fiction',
+         'Price':5,
+         'Quantity':1
+      },
+      {
+         'Name':'Robin Hobb - Assassin Apprentice',
+         'Category':'Fantastique',
+         'Price':12,
+         'Quantity':8
+      }
+   ]
+}";
+    }
+}
---
+++ BookStore/BookStore/BookStore.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace BookStore
+{
+    public class BookStore : IStore
+    {
+        public void ValidBasket(params string[] basketByNames)
+        {
+            var copies = new Dictionary<string, int>();
+            List<NameQuantity> missing = new List<NameQuantity>();
+
+            foreach (var item in basketByNames)
+            {
+                if (copies.ContainsKey(item))
+                {
+                    copies[item] = copies[item] + 1;
+                }
+                else
+                {
+                    copies.Add(item, 1);
+                }
+            }
+
+            foreach (var item in copies)
+            {
+                var book = StoreDataSource.StoreData.Catalog.FirstOrDefault(c => c.Name == item.Key);
+                if (book.Quantity < item.Value)
+                    missing.Add(new NameQuantity() { Name = book.Name, Quantity = book.Quantity });
+            }
+
+            if (missing.Any())
+                throw new NotEnoughInventoryException(missing);
+            
+        }
+        public double Buy(params string[] basketByNames)
+        {
+            ValidBasket(basketByNames);
+
+            if (basketByNames.Count() == 1)
+                return StoreDataSource.StoreData.Catalog.FirstOrDefault(c => c.Name == basketByNames[0])?.Price ?? 0;
+
+            var books = StoreDataSource.StoreData.Catalog.Where(c => basketByNames.Contains(c.Name));
+            var categories = books.Select(c => c.Category).Distinct();
+            var sameCategory = categories.Count() == 1;
+            var isDistinct = basketByNames.Distinct().Count() == basketByNames.Count();
+            if (sameCategory && isDistinct)
+            {
+                var category = categories.FirstOrDefault();
+                var discount = StoreDataSource.StoreData.Category.FirstOrDefault(c => c.Name == category).Discount;
+                return books.Sum(c => c.Price) * (1 - discount);
+            }
+
+           
+            double price = 0;
+            List<string> copies = new List<string>();
+            foreach (var item in basketByNames)
+            {
+                var book = StoreDataSource.StoreData.Catalog.FirstOrDefault(c => c.Name == item);
+                var otherBookInSameCategory = books.Where(c => c.Category == book.Category).Count() > 1;
+                if (!copies.Contains(book.Name) && otherBookInSameCategory)
+                {
+                    var discount = StoreDataSource.StoreData.Category.FirstOrDefault(c => c.Name == book.Category).Discount;
+                    price += book.Price * (1 - discount);
+                }
+                else
+                {
+                    price += book.Price;
+                }
+                copies.Add(item);
+            }
+
+            return price;
+        }
+
+        public void Import(string catalogAsJson)
+        {
+            StoreDataSource.StoreData = JsonConvert.DeserializeObject<StoreData>(catalogAsJson);
+        }
+
+        public int Quantity(string name)
+        {
+            return StoreDataSource.StoreData.Catalog.Where(c => c.Name == name).Sum(c => c.Quantity);
+        }
+    }
+}
